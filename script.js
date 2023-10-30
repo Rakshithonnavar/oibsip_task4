cc048d1f4626d0739b2718cf8011cc4309a27a7f
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-10-30 16:58:21 +0530

    latest commit

--- script.js
+++ script.js
@@ -50,6 +50,78 @@
         const rePassword = document.getElementById('password2').value;
         console.log(userName+passWord+rePassword);
     }
+
+
+
+    // <script>
+    //       function encryptData(data, key) {
+    //           const encrypted = CryptoJS.AES.encrypt(data, key);
+    //           return encrypted.toString();
+    //       }
+    //       const signup = document.getElementById("submitSignup").addEventListener("click", function (event) {
+    //           event.preventDefault();
+    //           saveData();
+    //       });
+
+    //       function saveData() {
+    //           const userName = document.getElementById('UserName').value;
+    //           const passWord = document.getElementById('password1').value;
+    //           const rePassword = document.getElementById('password2').value;
+
+    //           if (passWord === rePassword) {
+    //               let user_records = JSON.parse(localStorage.getItem("users")) || [];
+
+
+    //               const encryptedPassword = encryptData(passWord,'your-secret-key');
+
+    //               if (user_records.some((v) => {
+    //                   return (v.user === userName)
+    //               })) {
+    //                   alert("Account Already exists!!");
+    //               } else {
+    //                   user_records.push({
+    //                       "user": UserName,
+    //                       "password": encryptedPassword,
+    //                   });
+    //                   localStorage.setItem("users", JSON.stringify(user_records));
+    //                   window.location.href = "login.html";
+    //               }
+    //           } else {
+    //               alert("Password Mismatch!");
+    //           }
+    //       }
+    //   </script>
+
+
+
+//     <script>
+//        function decryptData(encryptedData, key) {
+//     const decryptedBytes = CryptoJS.AES.decrypt(encryptedData, key);
+//     return decryptedBytes.toString(CryptoJS.enc.Utf8);
+// }
+
+// const login = document.getElementById("submitLogin").addEventListener("click", function (event) {
+//     event.preventDefault();
+//     validateData();
+// });
+
+
+// function validateData() {
+//     const userName = document.getElementById('UserName').value;
+//     const passWord = document.getElementById('Password').value;
+//     let user_records = JSON.parse(localStorage.getItem("users")) || [];
+//     const user = user_records.find((v) => v.user === userName);
+
+//     if (user && decryptData(user.password, 'your-secret-key') === passWord) {
+//         alert("Login successful!!!");
+//         localStorage.setItem("name", user.user);
+//         localStorage.setItem("password", user.password);
+//         window.location.href = "tributepage.html";
+//     } else {
+//         alert("Login failed");
+//     }
+// }
+// </script>
     
 
 
