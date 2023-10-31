2702de3be16cff3c6983844b711edc0a9fc6ab73
Rakshit honnavar <rakshithonnavar20@gmail.com>
2023-11-01 01:16:39 +0530

    latest commit

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-const forms = document.querySelector(".forms"),
-pwShowHide = document.querySelectorAll(".eye-icon"),
-links = document.querySelectorAll(".link");
-
-pwShowHide.forEach(eyeIcon => {
-eyeIcon.addEventListener("click", () => {
-  let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-  
-  pwFields.forEach(password => {
-      if(password.type === "password"){
-          password.type = "text";
-          eyeIcon.classList.replace("bx-hide", "bx-show");
-          return;
-      }
-      password.type = "password";
-      eyeIcon.classList.replace("bx-show", "bx-hide");
-  })
-  
-})
-})      
-
-links.forEach(link => {
-link.addEventListener("click", e => {
- e.preventDefault(); //preventing form submit
- forms.classList.toggle("show-signup");
-})
-})
+function saveData()
+{
+    const userName = document.getElementById('userName').value;
+    const passWord = document.getElementById('password1').value;
+    const rePassword = document.getElementById('password2').value;
+    console.log(userName+passWord+rePassword);
+}
